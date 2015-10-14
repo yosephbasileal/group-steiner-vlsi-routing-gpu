fd5cfa4af5a816b2fae4264b354433cf2c98bb19
Basileal Imana <yosephbasileal@yahoo.com>
2015-10-13 21:06:56 -0400

    move remSpanned into twostar.h

--- twostar.h
+++ twostar.h
@@ -1,5 +1,4 @@
 #include "partial-star.h"
-#include "remSpanned.h"
 
 //Stores cost and root of solution twostar tree
 struct Solution {
@@ -14,6 +13,24 @@ struct TwoStar {
   int numPar;
   int* partialstars;
 };
+
+//Removes the alraedy spanned groups after each partial star is calculated
+void remSpanned(int* groupIds, int* parStar, int* newGroupsIds, int remGroups, int numGroups) {
+  int flag = 0;
+  for(int i = 0, j = 0; i < numGroups; i++) { //for each group
+    for(int k = 0; k < parStar[1]; k++) { //go through current partial star
+      if(groupIds[i] == parStar[2+k]) { //if already spanned dont include in the newGroup
+        flag = 1;
+        break;
+      }
+    }
+    if(flag) {
+      flag = 0;
+      continue;
+    }
+    newGroupsIds[j++] = groupIds[i];
+  }
+}
 
 /* 
  * Constructs a twostar tree given a root
